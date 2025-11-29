2672bee08c532d8749d482e292d34e2d2b9b8113
Stefan <1240279+stfnhh@users.noreply.github.com>
2025-11-29 11:20:07 -0500

    add pvc for redis, updated gha

--- apps/redis/deployment.tf
+++ apps/redis/deployment.tf
@@ -8,21 +8,35 @@ resource "kubernetes_deployment" "deployment" {
 
   spec {
     replicas = 1
+
     selector {
       match_labels = {
         app = "redis"
       }
     }
+
     template {
       metadata {
         labels = {
           app = "redis"
         }
       }
+
       spec {
         container {
           name  = "redis"
           image = "redis:alpine@sha256:6cbef353e480a8a6e7f10ec545f13d7d3fa85a212cdcc5ffaf5a1c818b9d3798"
+
+          command = ["redis-server"]
+          args = [
+            "--save", "60", "1000",
+            "--appendonly", "no"
+          ]
+
+          volume_mount {
+            name       = "redisdata"
+            mount_path = "/data"
+          }
 
           security_context {
             allow_privilege_escalation = false
@@ -31,26 +45,6 @@ resource "kubernetes_deployment" "deployment" {
             capabilities {
               drop = ["ALL", "NET_RAW"]
             }
-          }
-
-          readiness_probe {
-            exec {
-              command = ["redis-cli", "ping"]
-            }
-            initial_delay_seconds = 3
-            period_seconds        = 5
-            timeout_seconds       = 2
-            failure_threshold     = 3
-          }
-
-          liveness_probe {
-            exec {
-              command = ["redis-cli", "ping"]
-            }
-            initial_delay_seconds = 10
-            period_seconds        = 10
-            timeout_seconds       = 3
-            failure_threshold     = 5
           }
 
           resources {
@@ -66,6 +60,14 @@ resource "kubernetes_deployment" "deployment" {
 
           port {
             container_port = 6379
+          }
+        }
+
+        volume {
+          name = "redisdata"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.persistent_volume_claim.metadata[0].name
           }
         }
       }
