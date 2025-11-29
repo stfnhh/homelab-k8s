74481a39333f87598362806cbc84206dbb7c7ba2
Stefan <1240279+stfnhh@users.noreply.github.com>
2025-11-29 13:38:42 -0500

    fix checkov

--- platform/modules/cert_manager/iam.tf
+++ platform/modules/cert_manager/iam.tf
@@ -1,4 +1,5 @@
 data "aws_iam_policy_document" "iam_policy_document" {
+
   statement {
     actions = [
       "route53:GetChange"
@@ -27,10 +28,12 @@ data "aws_iam_policy_document" "iam_policy_document" {
 }
 
 resource "aws_iam_user" "iam_user" {
+  # checkov:skip=CKV_AWS_273:This IAM user is intentionally used for automated ACME DNS challenge authentication; SSO is not applicable
   name = "k3s-letsencrypt"
 }
 
 resource "aws_iam_user_policy" "iam_user_policy" {
+  # checkov:skip=CKV_AWS_40:This inline user policy is required for ACME DNS challenge automation; attaching to a role or group is not possible for the workflow
   name   = "k3s-letsencrypt"
   user   = aws_iam_user.iam_user.name
   policy = data.aws_iam_policy_document.iam_policy_document.json
