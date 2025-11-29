69ebedcc9951deecf27523af1feaaf191e6b4b3c
Stefan <1240279+stfnhh@users.noreply.github.com>
2025-11-29 12:59:34 -0500

    force test

--- apps/redis/deployment.tf
+++ apps/redis/deployment.tf
@@ -4,6 +4,8 @@ resource "kubernetes_deployment" "deployment" {
   # test
   # test
   # test
+  # test
+  # test
   metadata {
     name      = "redis"
     namespace = kubernetes_namespace.namespace.metadata[0].name
